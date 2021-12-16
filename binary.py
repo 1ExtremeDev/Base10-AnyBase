7ddcb0bedf3e69c34758a32e22844adac47d124b
ExtremeDev <79762562+1ExtremeDev@users.noreply.github.com>
2021-12-16 11:23:49 +0200

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,5 @@
+
+def base(num, base=2, binary=""):
+    while num > 0 and base <= 62: binary += "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[num % base]; num //= base
+    return binary[::-1] if not(len(binary) == 0) else "0"
+  
