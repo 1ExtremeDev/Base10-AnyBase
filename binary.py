fe4a83c7d80d1ba5c925cf9da3193ea9dce9eb58
ExtremeDev <79762562+1ExtremeDev@users.noreply.github.com>
2021-12-16 11:31:58 +0200

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,3 +1,3 @@
 def base(num, base=2, binary=""):
-    while num > 0 and base <= 62: binary += "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[num % base]; num //= base
-    return binary[::-1] if not(len(binary) == 0) else "0"
+ while num > 0 and base <= 62: binary += "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[num % base]; num //= base
+ return binary[::-1] if not(len(binary) == 0) else "0"
